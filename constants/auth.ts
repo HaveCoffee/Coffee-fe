bfe472a59badc8502825a0acde28fcadca461c97
Prince Maurya <princemaurya002@gmail.com>
2025-12-30 15:24:59 +0530

    Remove hardcoded secret

--- constants/auth.ts
+++
@@ -1,3 +0,0 @@
-export const AUTH_TOKEN_KEY = 'authToken';
-export const REFRESH_TOKEN_KEY = 'refreshToken';
-export const COFFEE_ML_JWT_SECRET = 'SUPER_SECRET_KEY_FOR_JWT_SIGNING'; // Must match the Coffee-ML backend's JWT_SECRET_KEY
